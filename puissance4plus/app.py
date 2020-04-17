7f4bb091f4d94b5f117716682165267e46939983
Tee <nico.J.64@hotmail.be>
2020-04-17 16:15:30 +0200

    Ajout ui et des éléments front-end

--- puissance4plus/app.py
+++ puissance4plus/app.py
@@ -3,21 +3,25 @@
 import os
 
 import mimetypes
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, render_template
+from flaskwebgui import FlaskUI
 
 mimetypes.init()
 app = Flask(__name__)
+ui = FlaskUI(app)
 
 
 @app.route("/resource/<path:path>")
 def get_resource(path):
     directories = {
-        "application/javascript": "js",
+        "application/js": "js",
         "text/css": "css",
         "image/png": "image",
         "image/jpeg": "image",
         "image/svg+xml": "image",
-        "audio/mpeg": "audio"
+        "audio/mpeg": "audio",
+        "audio/ogg": "audio",
+        "font/ttf": "font"
     }
     ext = os.path.splitext(path)[1]
     mimetype = mimetypes.types_map.get(ext, "text/html")
@@ -25,6 +29,10 @@ def get_resource(path):
     directory_path = os.path.join("resources", directory)
     return send_from_directory(os.path.join(app.static_folder, directory_path), path)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    ui.run()
