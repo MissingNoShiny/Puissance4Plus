d1782bc8cbd30308d74c878198d7ea4f95273e73
Tee <nico.J.64@hotmail.be>
2020-04-18 21:14:14 +0200

    Backend basique des menus et implémentation du plein écran

--- puissance4plus/app.py
+++ puissance4plus/app.py
@@ -3,13 +3,15 @@
 import os
 import sys
 
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, send_from_directory, render_template, redirect
 from webui import WebUI
 
 
 class UI(WebUI):
     def __init__(self, app, debug=False):
         super().__init__(app, debug=debug)
+        self.view.setWindowTitle('Puissance 4 SUPER')
+        self.view.setMinimumSize(1280, 720)
         self.view.showFullScreen()
 
 
@@ -45,21 +47,32 @@ class Game:
 
         @self.app.route("/")
         def mainMenu():
-            return render_template('main_menu.html')
+            status = "checked" if(self.ui.view.isFullScreen()) else "unchecked"
+            return render_template('main_menu.html', status=status)
 
-        @self.app.route("/gameOptions")
+        @self.app.route("/gameOptions", methods=['GET'])
         def gameOptionsMenu():
-            return render_template('game_options_menu.html')
+            return render_template('game_options_menu.html', mode=request.args.get('mode'))
 
         @self.app.route("/close")
         def close():
             self.stop()
+
+        @self.app.route("/settings", methods=['GET'])
+        def graphicalOptions():
+            self.toggleFullscreen(request.args.get('fullscreen'))
+            return redirect("/")
 
         self.ui.run()
 
     def stop(self):
         self.ui.view.close()
 
+    def toggleFullscreen(self, fullscreen):
+        if fullscreen:
+            self.ui.view.showFullScreen()
+        else:
+            self.ui.view.showNormal()
 
 if __name__ == "__main__":
     Game()
