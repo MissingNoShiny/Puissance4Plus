3ad01d2d23d8ba62e1e6e8659f9f35216704062d
Unknown <larcinvincent@gmail.com>
2020-04-18 12:28:20 +0200

    full screen + classe Game

--- puissance4plus/app.py
+++ puissance4plus/app.py
@@ -5,33 +5,42 @@ import os
 from flask import Flask, request, send_from_directory, render_template
 from webui import WebUI
 
-app = Flask(__name__)
-ui = WebUI(app, debug=True)
+
+class UI(WebUI):
+    def __init__(self, app, debug=False):
+        super().__init__(app, debug=debug)
+        self.view.showFullScreen()
 
 
-@app.route("/resource/<path:path>")
-def get_resource(path):
-    path = path.replace("/", os.path.sep)
-    directories = {
-        ".js": "js",
-        ".css": "css",
-        ".png": "image",
-        ".jpg": "image",
-        ".svg": "image",
-        ".ico": "image",
-        ".mp3": "audio",
-        ".ogg": "audio",
-        ".ttf": "font",
-    }
-    ext = os.path.splitext(path)[1]
-    directory = directories.get(ext, "")
-    return send_from_directory(os.path.join(app.static_folder, directory), path)
+class Game:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.ui = UI(self.app, debug=True)
 
+        @self.app.route("/resource/<path:path>")
+        def get_resource(path):
+            path = path.replace("/", os.path.sep)
+            directories = {
+                ".js": "js",
+                ".css": "css",
+                ".png": "image",
+                ".jpg": "image",
+                ".svg": "image",
+                ".ico": "image",
+                ".mp3": "audio",
+                ".ogg": "audio",
+                ".ttf": "font",
+            }
+            ext = os.path.splitext(path)[1]
+            directory = directories.get(ext, "")
+            return send_from_directory(os.path.join(self.app.static_folder, directory), path)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+        @self.app.route("/")
+        def index():
+            return render_template('index.html')
+
+        self.ui.run()
 
 
 if __name__ == "__main__":
-    ui.run()
+    Game()
