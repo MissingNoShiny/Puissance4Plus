0a106c733afce22e218769c18c1f8deb92221158
Mathias <matou-bil-14@hotmail.fr>
2020-04-20 14:54:44 +0200

    Menu game options upgrade

--- puissance4plus/static/js/game_options_menu.js
+++ puissance4plus/static/js/game_options_menu.js
@@ -8,33 +8,55 @@ $("input.slider[name=winLength]").on("input change", e => {
 // Class Player
 let maxPlayers = 5;
 let Players = [];
-class Player {
-    constructor() {
-        if(Players.length < maxPlayers) {
-            // Create and append node
-            this.elmt = $($("template.player")[0].content).clone().children(".player");
-            this.elmt.appendTo($(".players"));
-            // Add to players list
-            Players.push(this);
-            // Display id
-            this.id = Players.length;
-            this.elmt.find(".id").text(this.id);
-            // Event listeners : name
-            this.name;
-            this.elmt.children("[name=name]").on("input", e => {
-                this.name = e.target.value;
-                if(e.target.value.length >= 2 && e.target.value.length <= 10) {
-                    this.nameValid = true
-                } else this.nameValid = false
-            })
-            // Formatting select color
-            this.elmt.find("[name=color] option").each((i, e) => {
-                $(e).css("background", e.value)
-            })
-            this.elmt.find("[name=color]").change(e => {
-                $(e.target).css("background", $(e.target).val())
-            }).change();
-            console.log(Players);
-        } else return false;
+let Ids = [];
+for(let i = 1; i <= maxPlayers; i++) Ids.push(i);
+let Colors = {
+    1: "#fcd53d", 
+    2: "#ff0000",
+    3: "#24D12B",
+    4: "#E8578D",
+    5: "#00FFFF"
+};
+function newPlayer() {
+    if(Players.length < maxPlayers) {
+        let p = new Player(Ids.shift());
+        p.elmt.find(".delete").click(() => { p.delete() })
+        Players.push(p);
     }
 }
+class Player {
+    constructor(id) {
+        // Node
+        this.elmt = $($("template.player")[0].content).children(".player").clone(false);
+        this.elmt.appendTo(".players");
+        // Display id
+        this.id = id;
+        this.elmt.find(".id").text(this.id);
+        // Display color
+        this.color = Colors[this.id];
+        this.elmt.find(".color").css("background", this.color);
+        // Event listeners : name
+        this.name;
+        this.elmt.find(".name").on("input", e => {
+            this.name = e.target.value;
+            console.log(this.name);
+            if(e.target.value.length >= 2 && e.target.value.length <= 10) {
+                this.nameValid = true
+            } else this.nameValid = false
+        })
+    }
+    delete() {
+        this.elmt.remove();
+        Ids.push(this.id);
+        for(let i = 0; i < Players.length; i++) {
+            if(Players[i].id == this.id) Players.splice(i, 1);
+        }
+    }
+}
+// FOOTER BUTTONS
+$("footer .back").click(() => {
+    window.location.href = "/";
+})
+$("footer .play").click(() => {
+    // TODO PLAY
+})
